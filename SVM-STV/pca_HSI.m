468ad23eee347b36fec0a91e3b02401dfc68a03e
jasoncui <kangnicui2@gmail.com>
2022-05-25 17:24:31 +0800

    Add files via upload

--- SVM-STV/pca_HSI.m
+++ SVM-STV/pca_HSI.m
@@ -1,1 +1,10 @@
-
+function pca_img=pca_HSI(recon_HSI,variance)
+p=0;
+[coeff,score,latent,tsquared,explained] = pca(recon_HSI);
+ for i=1:size(recon_HSI,2)
+    if sum(explained(1:i))/sum(explained)>variance
+        p=i;
+        break
+    end
+end
+pca_img = score(:,1:p);
